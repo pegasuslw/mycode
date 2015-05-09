b80457f899ebcabe9b554d1722b05e08df06cb84
liuwei <pegasus.lw@gmail.com>
2015-05-09 22:03:02 +0800

    add HelloMoon Project

---
+++ java/android_app_practice/HelloMoon/src/com/tcl/hellomoon/AudioPlayer.java
@@ -0,0 +1,44 @@
+package com.tcl.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.view.SurfaceHolder;
+
+public class AudioPlayer {
+
+	private MediaPlayer mPlayer;
+	
+	public void play(Context context){
+		if(mPlayer == null){
+			mPlayer = MediaPlayer.create(context, R.raw.guangming);
+			//mPlayer = MediaPlayer.create(context, R.raw.apollo_17_stroll);
+			mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+				
+				@Override
+				public void onCompletion(MediaPlayer mp) {
+					mPlayer.stop();
+				}
+			});
+		}
+		
+		if(!mPlayer.isPlaying())  // 只有在没播放的时候，才调用start开始播放
+		{
+			mPlayer.start();
+		}
+
+	}
+	
+	public void stop(){
+		if (mPlayer != null) {
+			mPlayer.release();
+			mPlayer = null;
+		}
+	}
+	
+	public void setDisplay(SurfaceHolder holder){
+		if (mPlayer != null){
+			mPlayer.setDisplay(holder);
+		}
+		
+	}
+}
